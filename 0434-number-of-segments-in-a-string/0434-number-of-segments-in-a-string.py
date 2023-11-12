907961aad173b945b1ecee39f9dd53b55dd98d25
Pratham Kumar <pratham21405@iiitd.ac.in>
2023-11-13 01:48:56 +0530

    Time: 21 ms (10.55%), Space: 13.3 MB (56.42%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        segs=s.split()
+        return len(segs)
+        
