2347bdb1f8e4999502565cab3225ae8b0cb60e48
Pratham Kumar <pratham21405@iiitd.ac.in>
2023-11-13 01:47:31 +0530

    Time: 16 ms (42.48%), Space: 13.6 MB (24.30%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        words = s.split()
+        if not words:
+            return 0
+        last_word=words[-1]
+        return len(last_word)
