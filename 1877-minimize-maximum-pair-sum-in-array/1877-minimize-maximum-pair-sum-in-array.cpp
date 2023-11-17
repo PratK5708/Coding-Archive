6808cde92cc9b8fc079f54badb400009f4d118a3
Pratham Kumar <pratham21405@iiitd.ac.in>
2023-11-18 02:12:21 +0530

    Time: 241 ms (5.47%), Space: 97.9 MB (9.78%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -1,10 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <thread>
+#include <mutex>
+
 class Solution {
 public:
-    int minPairSum(vector<int>& A) {
+    int res;
+    std::mutex mtx;
+
+    void calculateMaxPairSum(std::vector<int>& A, int start, int end) {
+        int localMax = 0;
+        for (int i = start; i < end; ++i)
+            localMax = std::max(localMax, A[i] + A[A.size() - i - 1]);
+
+        std::lock_guard<std::mutex> lock(mtx);
+        res = std::max(res, localMax);
+    }
+
+    int minPairSum(std::vector<int>& A) {
         sort(A.begin(), A.end());
-        int res = 0, n = A.size();
-        for (int i = 0; i < n / 2; ++i)
-            res = max(res, A[i] + A[n - i - 1]);
+        res = 0;
+        int n = A.size();
+        int numThreads = std::thread::hardware_concurrency();
+        std::vector<std::thread> threads;
+
+        for (int i = 0; i < numThreads; ++i) {
+            int start = i * (n / numThreads);
+            int end = (i == numThreads - 1) ? n : (i + 1) * (n / numThreads);
+            threads.emplace_back(&Solution::calculateMaxPairSum, this, std::ref(A), start, end);
+        }
+
+        for (auto& thread : threads) {
+            if (thread.joinable())
+                thread.join();
+        }
+
         return res;
     }
 };
