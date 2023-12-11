8eb8bb1aed9aa3a14e1167e944782230e32c6a19
Pratham Kumar <pratham21405@iiitd.ac.in>
2023-12-11 14:53:19 +0530

    Time: 15 ms (26.10%), Space: 12.7 MB (72.53%) - LeetHub

---
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int n=arr.size();
+        int threshold=n / 4;
+
+        for (int i=0;i<n-threshold;++i){
+            if (arr[i]==arr[i+threshold]){
+                return arr[i];
+            }
+        }
+
+        return -1;
+    }
+};
