16b8fbd1eeba4e2f270764c14f5c92e69af99d29
Pratham Kumar <pratham21405@iiitd.ac.in>
2024-02-05 00:31:28 +0530

    Time: 14 ms (98.40%), Space: 12 MB (92.99%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def calPoints(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        
+        stack= deque() 
+        for i in operations:
+          if i=="C":
+            stack.pop()
+          elif i=="D":
+             stack.append(2*stack[-1])
+          elif i=="+":
+             stack.append(stack[-1]+stack[-2])
+          else:
+            stack.append(int(i))
+        return sum(stack)
